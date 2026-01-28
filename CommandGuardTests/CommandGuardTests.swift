6998b373e6dd728c69a635bc1fb5b52021612dd5
Luke Arsenault <larsenault2021@gmail.com>
2026-01-27 22:29:02 -0500

    Initial Commit

---
+++ CommandGuardTests/CommandGuardTests.swift
@@ -0,0 +1,17 @@
+//
+//  CommandGuardTests.swift
+//  CommandGuardTests
+//
+//  Created by Luke Arsenault on 1/27/26.
+//
+
+import Testing
+@testable import CommandGuard
+
+struct CommandGuardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
