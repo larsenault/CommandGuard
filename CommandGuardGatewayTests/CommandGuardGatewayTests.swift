91da1900da007a285ff67312b72f208a5d13f3bc
Luke Arsenault <larsenault2021@gmail.com>
2026-02-12 21:35:42 -0500

    Backend Project changes showing the addition of added files, etc.

---
+++ CommandGuardGatewayTests/CommandGuardGatewayTests.swift
@@ -0,0 +1,17 @@
+//
+//  CommandGuardGatewayTests.swift
+//  CommandGuardGatewayTests
+//
+//  Created by Luke Arsenault on 2/12/26.
+//
+
+import Testing
+@testable import CommandGuardGateway
+
+struct CommandGuardGatewayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
